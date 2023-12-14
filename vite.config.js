c95dcb22287b27771aa315d667d383a6573cc303
bad man <badman@badman.local>
2023-12-14 22:07:10 +0200

    add gh

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,6 @@ import { defineConfig } from 'vite';
 import { createSvgIconsPlugin } from 'vite-plugin-svg-icons';
 
 export default defineConfig ({
-  base: '/astra_nuvo/',
   root: path.resolve(__dirname, 'src'),
   build: {
     outDir: '../dist',
